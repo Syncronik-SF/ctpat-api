6eebea03467a42300522fd97d1ef1245c688d31e
Ramses Landero <27landero@gmail.com>
2022-09-11 21:19:07 -0500

    model3

--- ctpat/settings.py
+++ ctpat/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +44,7 @@ INSTALLED_APPS = [
     # Third apps
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_api_key',
+    'rest_framework_api_key',  
 
     # Custom applications
     'authentication',
@@ -147,4 +149,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "authentication.CustomUser"
 
 
-MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- incidence/models.py
+++ incidence/models.py
@@ -1,8 +1,8 @@
-from PIL import Image
 from django.db import models
 from authentication.models import CustomUser
 from forms.models import RevisionCanina, CheckList
 from django.core.validators import FileExtensionValidator
+
 
 
 
@@ -12,9 +12,10 @@ class Incidence(models.Model):
     title= models.CharField(max_length=50)
     incidencias = models.ForeignKey( CheckList, on_delete=models.DO_NOTHING)
     descripcion = models.TextField(default="",null=True, blank=True)
-    date = models.ForeignKey( CheckList, on_delete=models.DO_NOTHING)
+    date = models.DateField( auto_now_add=True, null=True, blank=True)
     hour = models.TimeField(auto_now_add=True)
-    #picture = models.FileField( 
-    #    upload_to=dynamic_path, 
-    #    validators=[FileExtensionValidator(['jpg', 'png'])], null= True, blank=True )
-    #---------------------------
+    picture = models.ImageField(upload_to="incidence"  ,blank=True, null=True)
+
+    def __str__(self):
+        return self.user
+    

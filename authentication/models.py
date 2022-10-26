edec312318f34796e2c861a7be2a244c052270c2
h-rub <rubio@loopgk.com>
2022-10-26 00:23:07 -0500

    IsOnline Field added (No views, only model, serializer and admin)

--- authentication/models.py
+++ authentication/models.py
@@ -14,6 +14,7 @@ class CustomUser(AbstractUser):
     email = models.EmailField(max_length=150, unique=True)
     phone = models.CharField(max_length=120, blank=True)
     job_title = models.CharField(max_length=40, null=True, blank=True)
+    is_online_in_app = models.BooleanField(default=False, null=True)
     
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)

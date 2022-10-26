96ec63d7878cc198832dbbdffb6b31f0b406522e
h-rub <rubio@loopgk.com>
2022-10-26 10:59:08 -0500

    Se agregó para que en la creación del embarque regrese el ID de Embarque creado.

--- forms/views.py
+++ forms/views.py
@@ -51,7 +51,7 @@ class CreateEmbarque(APIView):
         embarque = Embarque.objects.create(creado_por = user, guardia = guardia, operador = operador, linea_transporte = linea_transporte, marca_tractor = marca_tractor, 
         numero_placas_tractor = numero_placas_tractor, no_economico = no_economico, linea_de_caja = linea_de_caja, numero_caja = numero_caja, numero_placas_caja = numero_placas_caja, autorizado_por = autorizado_por,
         factura = factura, numero_pallets = numero_pallets, numero_sello = numero_sello, sello_entregado_a = sello_entregado_a, destino = destino, es_exportacion = es_exportacion)
-        return Response({"msg":"Form embarque been created"}, status=status.HTTP_200_OK)
+        return Response({"msg":"Form embarque been created", "idEmbarque": embarque.pk}, status=status.HTTP_200_OK)
 
 
 class CreateEntrada(APIView):

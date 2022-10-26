1eff45061459dee3e15e506fc0fafff0b6b3d510
Luiggi <luirami98@gmail.com>
2022-10-25 21:07:52 -0500

    Añadido endpoint que lista todos los usuarios

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import CustomUserView, LoginView, LogoutView, MembersView, ProfilePictureView, SignUpView, UpdateProfileView, UserProfileView, upload_photo
+from .views import CustomUserView, LoginView, LogoutView, MembersView, ProfilePictureView, SignUpView, UpdateProfileView, UserProfileView, upload_photo, ListUsers
 
 from rest_framework.routers import DefaultRouter
 
@@ -23,5 +23,6 @@ urlpatterns = [
     path('profile/upload_photo', upload_photo, name="upload_photo"),
     path('profile/update', UpdateProfileView.as_view(), name="update_profile"),
     path('profile-pic/<int:id>', ProfilePictureView.as_view(), name="my_photo"),
-    path('members/all', MembersView.as_view({'get': 'list'}), name="members")
+    path('members/all', MembersView.as_view({'get': 'list'}), name="members"),
+    path('users', ListUsers.as_view()),
 ]
